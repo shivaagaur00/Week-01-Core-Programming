7e28f6c947b3785cfe0bd294c6a418018f4647da
shivaagaur00 <shivagaur1100@gmail.com>
2025-06-12 10:05:15 +0530

    Q2 completed

---
+++ Java Programming Elements/Q2.java
@@ -0,0 +1,7 @@
+
+class Q2{
+public static void main(String[] args){
+int pens=14,students=3;
+System.out.println("The Pen Per Student is "+(pens/students)+" and the remaining pen not distributed is "+(pens%students));
+}
+}
