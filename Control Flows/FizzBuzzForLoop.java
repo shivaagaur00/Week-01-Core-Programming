93b6de09226c9ce29200028c16af670606f68ed1
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-13 16:24:40 +0530

    [ShivaGaur] Fixes . FizzBuzzForLoop completed

--- Control Flows/FizzBuzzForLoop.java
+++ Control Flows/FizzBuzzForLoop.java
@@ -9,7 +9,7 @@ public class FizzBuzzForLoop {
             System.out.println("Invalid input");
             return;
         }
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n;i++) {
             if (i % 15 == 0) System.out.println("FizzBuzz");
             else if (i % 3 == 0) System.out.println("Fizz");
             else if (i % 5 == 0) System.out.println("Buzz");
