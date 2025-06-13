5a5bb0f0a971a3b2863b3743884c3458e880df82
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-13 16:17:08 +0530

    [ShivaGaur] Fixes . RocketCountdownWhileLoop

--- Control Flows/RocketCountdownWhileLoop.java
+++ Control Flows/RocketCountdownWhileLoop.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class RocketCountdownWhileLoop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter countdown start: ");
+        System.out.print("Enter countdown start:");
         int counter = sc.nextInt();
         while (counter >= 1) {
             System.out.println(counter);
