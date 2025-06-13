da89f475b12a710d78da956c5a02d1f1a83e4931
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-13 16:24:07 +0530

    [ShivaGaur] Fixes . EmployeeBonus completed

--- Control Flows/FizzBuzzForLoop.java
+++ Control Flows/FizzBuzzForLoop.java
@@ -9,7 +9,11 @@ public class FizzBuzzForLoop {
             System.out.println("Invalid input");
             return;
         }
+<<<<<<< HEAD
         for (int i = 1; i <= n;i++) {
+=======
+        for (int i = 1; i <= n; i++) {
+>>>>>>> 2fe003a ([ShivaGaur] Fixes . EmployeeBonus completed)
             if (i % 15 == 0) System.out.println("FizzBuzz");
             else if (i % 3 == 0) System.out.println("Fizz");
             else if (i % 5 == 0) System.out.println("Buzz");
