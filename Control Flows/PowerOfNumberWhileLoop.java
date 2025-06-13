1aef61fdd6ffb4233d174e38abec749f94bab0c5
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-13 16:39:44 +0530

    [ShivaGaur] Add . PowerOfNumberWhileLoop completed

---
+++ Control Flows/PowerOfNumberWhileLoop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PowerOfNumberWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base number: ");
+        int number = sc.nextInt();
+        System.out.print("Enter power: ");
+        int power = sc.nextInt();
+        long result = 1;
+        int counter = 0;
+        while (counter < power) {
+            result *= number;
+            counter++;
+        }
+        System.out.println(number + "^" + power + " = " + result);
+    }
+}
