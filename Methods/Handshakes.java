25504096af8e29560ff2a5c1f78833786580d84d
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-17 11:19:49 +0530

    [ShivaGaur] Add . Handshakes completed

--- Methods/Handshakes.java
+++ Methods/Handshakes.java
@@ -4,7 +4,6 @@ public class Handshakes {
     public static int calculateHandshakes(int n) {
         return n * (n - 1) / 2;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of students: ");
