976220cbe4dcd418881404486e95205b57a9b64e
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-17 11:13:38 +0530

    [ShivaGaur] Add . TrigonometricCalculator completed

--- Methods/TrigonometricCalculator.java
+++ Methods/TrigonometricCalculator.java
@@ -3,21 +3,14 @@ import java.util.Scanner;
 public class TrigonometricCalculator {
     public static double[] calculateTrigonometricFunctions(double angle) {
         double radians = Math.toRadians(angle);
-        double[] results = new double[3];
-        results[0] = Math.sin(radians);
-        results[1] = Math.cos(radians);
-        results[2] = Math.tan(radians);
-        return results;
+        return new double[]{Math.sin(radians), Math.cos(radians), Math.tan(radians)};
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter angle in degrees: ");
         double angle = sc.nextDouble();
-        
-        double[] trigResults = calculateTrigonometricFunctions(angle);
-        System.out.println("Sin: " + trigResults[0]);
-        System.out.println("Cos: " + trigResults[1]);
-        System.out.println("Tan: " + trigResults[2]);
+        double[] results = calculateTrigonometricFunctions(angle);
+        System.out.println("Sin: " + results[0] + " Cos: " + results[1] + " Tan: " + results[2]);
     }
 }
