b4b28f849d2d19bfce46519d2b3840f013c2f7d4
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-17 11:23:27 +0530

    [ShivaGaur] Add . MinMaxFinder completed

--- Methods/MinMaxFinder.java
+++ Methods/MinMaxFinder.java
@@ -4,7 +4,6 @@ public class MinMaxFinder {
     public static int[] findMinMax(int a, int b, int c) {
         return new int[]{Math.min(Math.min(a, b), c), Math.max(Math.max(a, b), c)};
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter three numbers: ");
