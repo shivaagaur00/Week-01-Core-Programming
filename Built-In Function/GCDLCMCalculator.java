49b294d62e63ccaf842a50234b956febea4034c7
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-20 11:02:40 +0530

    [ShivaGaur] Add . GCDLCMCalculator completed

---
+++ Built-In Function/GCDLCMCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GCDLCMCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("GCD: " + gcd(a, b));
+        System.out.println("LCM: " + lcm(a, b));
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+}
