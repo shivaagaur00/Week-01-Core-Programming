184cc32fbc343b24f13596c8408d9e00e013d5d0
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-25 10:09:18 +0530

    [ShivaGaur] Add . Hotel4 completed

--- Object-Oriented-Programming/1. Java Constructors/HotelBooking.java
+++ Object-Oriented-Programming/1. Java Constructors/HotelBooking.java
@@ -3,21 +3,18 @@ public class HotelBooking {
     String roomType;
     int nights;
 
-    // Default constructor
     public HotelBooking() {
         this.guestName = "Unknown";
         this.roomType = "Standard";
         this.nights = 1;
     }
 
-    // Parameterized constructor
     public HotelBooking(String guestName, String roomType, int nights) {
         this.guestName = guestName;
         this.roomType = roomType;
         this.nights = nights;
     }
 
-    // Copy constructor
     public HotelBooking(HotelBooking other) {
         this.guestName = other.guestName;
         this.roomType = other.roomType;
