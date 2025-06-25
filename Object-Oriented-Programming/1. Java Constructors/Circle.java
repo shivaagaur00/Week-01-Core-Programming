74d36bb976d2af133b6494011c9664c0ec90bac7
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-25 10:08:17 +0530

    [ShivaGaur] Add . Circle completed

--- Object-Oriented-Programming/1. Java Constructors/Circle.java
+++ Object-Oriented-Programming/1. Java Constructors/Circle.java
@@ -1,12 +1,9 @@
 public class Circle {
     double radius;
 
-    // Default constructor
     public Circle() {
-        this(1.0); // Constructor chaining
+        this(1.0); 
     }
-
-    // Parameterized constructor
     public Circle(double radius) {
         this.radius = radius;
     }
